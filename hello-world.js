d2a05eda8e3ca0659d45908c9fa82a1a09bbb411
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-16 19:55:11 +0000

    exercise 2

--- hello-world.js
+++ hello-world.js
@@ -3,3 +3,14 @@
 console.log('Hello-World!');
 
 setTimeout(function() {console.log('Hello World Again!!')}, 10000);
+
+
+// Wild interval
+
+setInterval(function() {
+
+  console.log('Hello World!');
+
+}, 10000);
+
+
