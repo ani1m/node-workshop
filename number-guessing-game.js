34908d817e3de95db94b42becfc0555b86a17079
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-17 20:23:37 +0000

    guessing game

--- number-guessing-game.js
+++ number-guessing-game.js
@@ -7,4 +7,37 @@ and any external module. HINT: there is an npm library called prompt that can
 help you with that :)
 */
 
-var prompt = require("prompt")
+var prompt = require('prompt')
+var randomNumber = Math.floor(Math.random() * 100 + 1)
+var  tries= 4;
+
+function game(tries) {
+prompt.get(["Pick a number from 1 to 100;"], function(err, res) {
+    err ? console.log(err) : null
+    
+    var num = Number(res["Pick a number from 1 to 100;"])
+    
+    if(num === randomNumber) {
+        console.log("You win!")
+        return
+    }
+    if(tries <=
+    1) {
+        console.log("game over! No more tries left") 
+        return
+    }
+    
+     if(num > randomNumber) {
+            console.log("Too low")
+            tries--
+        game(tries)
+        }
+        if(num < randomNumber) {
+            console.log("Too high")
+            tries--
+    game(tries)
+        }
+    }
+    
+ )}
+ game(tries)
