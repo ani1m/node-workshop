65e4857a4532d38c8b23e4bc269494c37f460c18
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-20 22:01:04 +0000

    iss-augmented using promises

--- iss-augmented.js
+++ iss-augmented.js
@@ -1,5 +1,6 @@
 // augmented iss application
 
+/*
 Number.prototype.toRadians = function() {
 return this * Math.PI / 180;
   }
@@ -56,4 +57,62 @@ prompt.get(['location'], function(err, result){
         })
     }
 })
+*/
 
+/*
+=======================================
+Augmented ISS with promises
+=======================================
+*/
+
+
+Number.prototype.toRadians = function() {
+    return this * Math.PI / 180;
+}
+
+var request = require('request-promise');
+var prompt = require('prompt-promise');
+
+var long;
+var lat;
+
+function coordinates() {
+
+    prompt('location')
+        .then(function(location) {
+            return request("https://maps.googleapis.com/maps/api/geocode/json?address=" + location)
+        })
+        .then(function(googleResponse) {
+            var parsedRes = JSON.parse(googleResponse)
+            lat = parseFloat(parsedRes.results[0].geometry.location.lat)
+            long = parseFloat(parsedRes.results[0].geometry.location.lng)
+            return request("http://api.open-notify.org/iss-now.json")
+        })
+        .then(function(issResponse) {
+            var searchResults = JSON.parse(issResponse)
+            var long2 = parseFloat(searchResults.iss_position.longitude)
+            var lat2 = parseFloat(searchResults.iss_position.latitude)
+
+            
+            var R = 6371e3; // metres
+            var φ1 = lat.toRadians();
+            var φ2 = lat2.toRadians();
+            var Δφ = (lat2 - lat).toRadians();
+            var Δλ = (long2 - long).toRadians();
+
+            var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+            var d = R * c;
+
+            console.log("Your distance from ISS is " + (d / 1000).toFixed(0) + "kms");
+        })
+        .catch(function(e){
+            console.log(e)
+        })
+        
+}
+
+coordinates()
