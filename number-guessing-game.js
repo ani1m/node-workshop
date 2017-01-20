acc5c914e16a4b8aaf63b8830388e8b259b4ee96
Ani Malkhassian <ani_mal@videotron.ca>
2017-01-20 17:52:46 +0000

    added promise format

--- number-guessing-game.js
+++ number-guessing-game.js
@@ -7,6 +7,7 @@ and any external module. HINT: there is an npm library called prompt that can
 help you with that :)
 */
 
+/*
 var prompt = require('prompt')
 
 //set computer randomNumber from 1-100
@@ -16,7 +17,7 @@ var randomNumber = Math.floor(Math.random() * 100 + 1)
 var userChoice = 4;
 
 
-function game(guess) {
+function game() {
     
     prompt.get(["Pick a number from 1 to 100"], function(err, res) {
             err ? console.log(err) : null
@@ -27,21 +28,68 @@ function game(guess) {
                 console.log("You win!")
                 return
             }
-            if (guess < 1) {
+            if (userChoice < 1) {
                 console.log("game over! No more tries left")
                 return
             }
 
             if (num > randomNumber) {
                 console.log("Too high")
-                guess--
-                game(guess)
+                userChoice--
+                game()
             }
             if (num < randomNumber) {
                 console.log("Too low")
-                guess--
-                game(guess)
+                userChoice--
+                game()
             }
         })
 }
+game(userChoice)
+
+*/
+// =========================================
+//           with promises
+// =========================================
+
+
+
+var prompt = require('prompt-promise')
+
+//set computer randomNumber from 1-100
+var randomNumber = Math.floor(Math.random() * 100 + 1)
+
+//number of tries given
+var userChoice = 4;
+
+
+function game() {
+    return prompt("Guess")
+        .then(function(num) {
+
+            //   var num = Number(res["Pick a number from 1 to 100"])
+
+            num = Number(num)
+            if (num === randomNumber) {
+                console.log("You win!")
+                return
+            }
+            if (userChoice < 1) {
+                console.log("game over! No more tries left")
+                return
+            }
+
+            if (num > randomNumber) {
+                console.log("Too high")
+                userChoice--
+                game()
+            }
+            if (num < randomNumber) {
+                console.log("Too low")
+                userChoice--
+                game()
+            }
+        })
+}
+
 game(userChoice)
